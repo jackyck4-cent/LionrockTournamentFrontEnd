39a0082e29a61d8d0cb56d3063de70f77228101e
vyonnelau <113070512+vyonnelau@users.noreply.github.com>
2022-11-28 22:05:20 -0500

    update of 28Nov
    - update bout-girl-small to use div instead of table
    - create new dummy files for bouts

---
+++ src/app/shared/bouts_db_info.ts
@@ -0,0 +1,11 @@
+export class BoutsInfoDB {
+    tn_id!: string;
+    round_id!: number;
+    match_id!: number;
+    player_1!: string;
+    player_2!: string;
+    score_1!: number;
+    score_2!: number;
+    winner!: string;
+    status!: string;
+}
