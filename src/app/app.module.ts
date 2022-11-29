c71f98f1c59640f05806c954c377247d296905fa
Yuk Ming Siu <ysiu4@my.centennialcollege.ca>
2022-11-28 23:15:14 -0500

    use buttons to show players in bout-grid-small component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,18 +19,21 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {WelcomePageComponent} from './pages/welcome-page/welcome-page.component';
 import {TnListPageComponent} from './pages/tn-list-page/tn-list-page.component';
 import {TnListComponent} from './components/tn-list/tn-list.component';
+import {TnDetailPageComponent} from './pages/tn-detail-page/tn-detail-page.component';
+import {TnDetailComponent} from './components/tn-detail/tn-detail.component';
+import { TnManagePageComponent } from './pages/tn-manage-page/tn-manage-page.component';
+import { TnCreatePageComponent } from './pages/tn-create-page/tn-create-page.component';
+
 import {MatTableModule} from '@angular/material/table';
 import {MatButtonModule} from '@angular/material/button';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatSortModule} from '@angular/material/sort';
-import {TnDetailPageComponent} from './pages/tn-detail-page/tn-detail-page.component';
 import {BoutGridSmallComponent} from './components/bout-grid-small/bout-grid-small.component';
 import {MatInputModule} from '@angular/material/input';
-import {TnDetailComponent} from './components/tn-detail/tn-detail.component';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import { TnManagePageComponent } from './pages/tn-manage-page/tn-manage-page.component';
-import { TnCreatePageComponent } from './pages/tn-create-page/tn-create-page.component';
+import {MatDividerModule} from '@angular/material/divider';
+
 
 @NgModule({
   declarations: [
@@ -65,6 +68,7 @@ import { TnCreatePageComponent } from './pages/tn-create-page/tn-create-page.com
     MatInputModule,
     MatRadioModule,
     MatFormFieldModule,
+    MatDividerModule,
   ],
   providers: [
     {
