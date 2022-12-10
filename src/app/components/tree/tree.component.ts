4f88a04b6357cfbe6254c516fcdeae95df151730
jackyck4 <jackyck4@gmail.com>
2022-12-09 20:47:04 -0500

    tree update

--- src/app/components/tree/tree.component.ts
+++ src/app/components/tree/tree.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { TnInfo } from 'src/app/shared/tn_info';
+import { PlayerInfo } from 'src/app/shared/player_info';
 
 @Component({
   selector: 'app-tree',
@@ -11,10 +12,20 @@ export class TreeComponent implements OnInit {
   @Input() 
   tn_info!:TnInfo;
 
+  @Input() 
+  players_info!: Map<string, PlayerInfo>;
+
   constructor() { }
 
   ngOnInit(): void {
     console.log(this.tn_info )
+  }
+
+  getUserName(user_id:string): string {
+    if (user_id != "")
+      return this.players_info.get(user_id)?.name ?? '---';
+    else
+      return '---';
   }
 
 }
